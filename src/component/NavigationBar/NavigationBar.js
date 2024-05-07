01841c68d6b2dbf07e2b08b1013833a9d8ce1947
Jordan Bruce <jeb79@njit.edu>
2024-05-07 12:01:05 -0400

    Got the navigation bar effect when hovering working.

--- src/component/NavigationBar/NavigationBar.js
+++ src/component/NavigationBar/NavigationBar.js
@@ -1,64 +1,32 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect } from "react";
 
 const NavLink = (props) => {
-  const { navLinkName, setLinkName } = props;
-
-  function handleMouseHover() {
-    setLinkName(navLinkName);
-  }
-
-  function handleMouseLeave() {
-    setLinkName(null);
-  }
+  const { navLinkName } = props;
 
   return (
     <li><a
           className="navbar-link"
           id={navLinkName}
           href={navLinkName}
-          onMouseEnter={handleMouseHover}
-          onMouseLeave={handleMouseLeave}
         > {navLinkName}
     </a></li>
   );
 }
 
 const NavigationBar = () => {
-  const [linkName, setLinkName] = useState(null);
-
   const navLinks = ['Home', 'About', 'Skills', 'Portfolio'];
   const navLinkMap = navLinks.map((link) => {
     return (
-      <NavLink navLinkName={link} setLinkName={setLinkName} />
+      <NavLink navLinkName={link} />
     );
   });
 
-  function changeColorLink(linkName) {
-    const navLinks = document.getElementsByClassName('navbar-link');
-
-    if (!linkName) {
-      for (const navLink of navLinks) {
-        if (navLink.id !== linkName) {
-          navLink.style.color = 'white';
-        }
-      }
-    } else {
-      for (const navLink of navLinks) {
-        if (navLink.id !== linkName) {
-          navLink.style.color = 'grey';
-        }
-      }
-    }
-  }
-
   useEffect(() => {
-    changeColorLink(linkName);
 
-  }, [linkName]);
+  }, []);
 
   return (
     <div className="navbar-container">
-      <h1>{linkName}</h1>
       <ul>
         {navLinkMap}
       </ul>
