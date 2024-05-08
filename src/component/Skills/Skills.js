4320412c76d43752f857074b2505fa220d2b0ddc
Jordan Bruce <jeb79@njit.edu>
2024-05-08 14:20:04 -0400

    Made additional css styling choices for skills card.

--- src/component/Skills/Skills.js
+++ src/component/Skills/Skills.js
@@ -7,6 +7,15 @@ import useWindowDimensions from '../../HelperFunctions'
 
 //image logo imports go here
 import html5 from "../../skill_icons/html5.png";
+import aws from "../../skill_icons/aws.png";
+import bootstrap from "../../skill_icons/bootstrap.png";
+import c from "../../skill_icons/c.png";
+import flask from "../../skill_icons/flask.png";
+import java from "../../skill_icons/java.png";
+import js from "../../skill_icons/js.png";
+import react from "../../skill_icons/react.png";
+import sql from "../../skill_icons/sql.png";
+import python from "../../skill_icons/python.png";
 
 const PageTitle = () => {
 
@@ -14,7 +23,7 @@ const PageTitle = () => {
     <div className="page-title">
       <p className="skills-subtitle"> <span style={{color: 'rgb(0,122,255)'}}>/ </span>MY SKILLS</p>
       <div className="bottom-container">
-        <h2 className="skills-header">My list of technical skills</h2>
+        <h2 className="skills-header">Programming Technologies</h2>
         <ScrollButtons />
       </div>
     </div>
@@ -70,41 +79,36 @@ const ScrollButtons = () => {
 const SkillCard = ({props}) => {
   const { skill, description, logo } = props;
 
-  const cardStyle = {
-    position: 'relative',
-    width: '425px',
-    height: '470px',
-    borderRadius: '7%',
-    margin: 'auto 15px'
-  }
   const cardImageStyle = {
     width: '20%',
     height: 'auto'
   }
 
   return (
-    <Card className="skill-card" style={cardStyle}>
+    <Card className="skill-card">
       <Card.Img variant="top" src={logo} style={cardImageStyle} ></Card.Img>
       <Card.Body>
-        <Card.Title>{skill}</Card.Title>
-        <Card.Text>{description}</Card.Text>
+        <Card.Title className="skill-card-title">{skill}</Card.Title>
+        <Card.Text className="skill-card-description">{description}</Card.Text>
       </Card.Body>
     </Card>
   );
 }
 
 const Skills = () => {
+
   const { height, width } = useWindowDimensions();
   const skillsData = [
     {skill: 'HTML & CSS', description: 'Industry standard markup language for documents displayed in a web browser.', logo: html5},
-    {skill: 'JavaScript', description: 'Programming language typically used alongside HTML and CSS on the client side for web appplications.'},
-    {skill: 'React JS', description: 'Popular frontend JavaScript library used to develop mobile and web applications.'},
-    {skill: 'React Bootstrap', description: 'Dynamic frontend framework that combines the power of React with the creativity of Bootstrap (CSS framework).'},
-    {skill: 'Flask', description: 'Microservice framework for Python that provides versatility for server-side functionality.'},
-    {skill: 'Python', description: 'High-level programming language known for usability in software applications, cybersecuirty, etc.'},
-    {skill: 'Java', description: 'A programming language used widely for object oriented programming and server-side functionality.'},
-    {skill: 'C++', description: 'Programming language commonly used in developing embedded systems and operating systems.'},
-    {skill: 'SQL', description: 'The standard language for relational database creation and manipulation.'}
+    {skill: 'JavaScript', description: 'Programming language typically used alongside HTML and CSS on the client side for web appplications.', logo: js},
+    {skill: 'React JS', description: 'Popular frontend JavaScript library used to develop mobile and web applications.', logo: react},
+    {skill: 'React Bootstrap', description: 'Dynamic frontend framework that combines the power of React with the creativity of Bootstrap (CSS framework).', logo: bootstrap},
+    {skill: 'Flask', description: 'Microservice framework for Python that provides versatility for server-side functionality.', logo: flask},
+    {skill: 'Python', description: 'High-level programming language known for usability in software applications, cybersecuirty, etc.', logo: python},
+    {skill: 'Java', description: 'A programming language used widely for object oriented programming and server-side functionality.', logo: java},
+    {skill: 'C++', description: 'Programming language commonly used in developing embedded systems and operating systems.', logo: c},
+    {skill: 'SQL', description: 'The standard language for relational database creation and manipulation.', logo: sql},
+    {skill: 'Amazon Web Services', description: 'Cloud computing platform for web hosting and other cloud service needs.', logo: aws}
   ];
   const skillsMap = skillsData.map((skill) => {
     return (
@@ -122,7 +126,7 @@ const Skills = () => {
   return (
     <div className="skills-page" style={style}>
       <PageTitle />
-      <table className="skills-table">
+      <table className="skills-table" style={{width: 'auto'}}>
         <tr>{skillsMap}</tr>
       </table>
     </div>
