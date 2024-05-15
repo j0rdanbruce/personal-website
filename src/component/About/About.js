a6b8823e8609f3a3b20b264c508e32b7f1893b28
Jordan Bruce <jeb79@njit.edu>
2024-05-15 16:51:37 -0400

    Working on css styling for information tabs.

--- src/component/About/About.js
+++ src/component/About/About.js
@@ -2,13 +2,39 @@ import React, { useState } from "react";
 import './About.css';
 
 import useWindowDimensions from '../../HelperFunctions';
-import { tab } from "@testing-library/user-event/dist/tab";
+
+//image imports go here
+import highlander from "../../Images/about_images/highlander.png";
+import scary_movie from "../../Images/about_images/scary_movie.png";
+import swimming from "../../Images/about_images/swimming.png";
+import teaching from "../../Images/about_images/teaching.png";
+import videogames from "../../Images/about_images/videogames.png";
 
 const Information = () => {
   const informationData = new Map();
-  informationData.set("Extracurriculars", {"description": "volutneer code instructor.."});
-  informationData.set("School", {"description": "I go to NJIT."});
-  informationData.set("Hobbies", {"description": "I swim."});
+  informationData.set("Extracurriculars", [
+    {description: "volutneer code instructor..",
+     image: teaching
+    }
+  ]);
+  informationData.set("School", [
+    {description: "I go to NJIT.",
+     image: highlander
+    }
+  ]);
+  informationData.set("Hobbies", [
+    {description: "I swim.",
+     image: swimming
+    },
+    {description: "I like to play video games.",
+     image: videogames
+    },
+    {description: "I am an avid movie watcher, especially scary movies. My favorite horror movie is an Alien movie!",
+     image: scary_movie
+    }
+  ]);
+
+  const [currentTab, setCurrentTab] = useState(informationData.get("Hobbies"));
 
   const infoDataEntries = [...informationData.keys()].map((infoKey) => {
     return (
@@ -16,7 +42,18 @@ const Information = () => {
     );
   });
 
-  const [currentTab, setCurrentTab] = useState(informationData.get("Hobbies"));
+  const infoImageStyle = {
+    width: '10%',
+    height: 'auto'
+  }
+  const currentInfoData = currentTab.map((info) => {
+    return(
+      <table className="table-info-container">
+        <tr><p className="table-description">{info.description}</p></tr>
+        <tr><div className="table-image-container"><img src={info.image} alt="" /></div></tr>
+      </table>
+    );
+  });
 
   function handleButtonClick (event) {
     const tabName = event.target.value;
@@ -30,7 +67,7 @@ const Information = () => {
         {infoDataEntries}
       </ul>
       <div className="information-container">
-        <p>{currentTab.description}</p>
+        {currentInfoData}
       </div>
     </div>
   );
